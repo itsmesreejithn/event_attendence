1262b82f9ccadccafe1b84677ac922dfc0ef95a0
Sreejith N <jithupreethy@gmail.com>
2024-02-05 18:02:31 +0530

    exporting participants router

--- app/app.js
+++ app/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 const eventRouter = require("./routers/eventRouter");
+const participantRouter = require("./routers/particaipantRouter");
 
 app.use(express.json());
 
 app.use("/api/v1/events", eventRouter);
+app.use("api/v1/participants", participantRouter);
 
 module.exports = app;
--- app/routers/particaipantRouter.js
+++ app/routers/particaipantRouter.js
@@ -7,3 +7,5 @@ router
   .route("/")
   .get(participantController.getAllParticipants)
   .post(participantController.createParticipant);
+
+module.exports = router;
